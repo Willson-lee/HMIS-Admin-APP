8cd9a37fa200a9f9ec08e86b528a923ae1e413fd
Sri <srivathsan@oasys.co>
2021-06-24 13:57:59 +0530

    ui

--- app/src/main/java/com/hmis_tn/admin/home/view/HomeActivity.kt
+++ app/src/main/java/com/hmis_tn/admin/home/view/HomeActivity.kt
@@ -2,11 +2,25 @@ package com.hmis_tn.admin.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hmis_tn.admin.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        initViews()
+        listeners()
+    }
+
+    private fun initViews() {
+        rvInstitutions?.layoutManager = LinearLayoutManager(this)
+        val adapter
+    }
+
+    private fun listeners() {
+
     }
 }
