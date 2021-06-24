3fcf56559f93ff5909bd843b22f1329f0078161d
Sri <srivathsan@oasys.co>
2021-06-24 13:00:12 +0530

    ui

---
+++ app/src/main/java/com/hmis_tn/admin/home/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.hmis_tn.admin.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hmis_tn.admin.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/hmis_tn/admin/login/view/LoginActivity.kt
+++ app/src/main/java/com/hmis_tn/admin/login/view/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.hmis_tn.admin.login.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.hmis_tn.admin.R
+import com.hmis_tn.admin.home.view.HomeActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun listeners() {
-
+        btnLogin?.setOnClickListener {
+            if (etUserName?.text?.toString() == "admin" && etPwd.text.toString() == "admin") {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                val sb = Snackbar.make(
+                    coordinatorLayout,
+                    "Please check the username and password",
+                    Snackbar.LENGTH_SHORT
+                )
+                sb.show()
+            }
+        }
     }
 }
