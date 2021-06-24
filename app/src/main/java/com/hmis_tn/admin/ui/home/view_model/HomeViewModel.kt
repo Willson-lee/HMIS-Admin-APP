fa32745a238465969141fa6b282ea65a9ac3823b
Sri <srivathsan@oasys.co>
2021-06-24 16:41:39 +0530

    ui

--- app/src/main/java/com/hmis_tn/admin/ui/home/view_model/HomeViewModel.kt
+++ app/src/main/java/com/hmis_tn/admin/ui/home/view_model/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.hmis_tn.admin.network.ApiService
 import com.hmis_tn.admin.network.NetworkClient
+import com.hmis_tn.admin.ui.home.model.OpListResp
 import com.hmis_tn.admin.utils.ProgressUtil
 import retrofit2.Callback
 
@@ -11,7 +12,7 @@ class HomeViewModel : ViewModel() {
 
     private lateinit var apiService: ApiService
 
-    fun getOpList(context: Context, callback: () -> ???) {
+    fun getOpList(context: Context, callback: Callback<OpListResp>) {
         ProgressUtil.startProgressDialog(context)
         apiService = NetworkClient.getNetworkClient()
         val call = apiService.getOpList()
